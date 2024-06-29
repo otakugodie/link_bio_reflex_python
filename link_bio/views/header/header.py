4b6481535a3bb25df5f66ca2dc3eff3332bff40c
otakugodie <95259091+otakugodie@users.noreply.github.com>
2024-06-29 17:46:52 +0100

    Styling elements title and links 2

--- link_bio/views/header/header.py
+++ link_bio/views/header/header.py
@@ -1,14 +1,22 @@
 import reflex as rx
 
 def header() -> rx.Component:
-    return rx.vstack(
+    return (rx.vstack(
         rx.hstack(
             rx.avatar(fallback="DR", size='5'),
             rx.vstack(
-                rx.heading('Diego Rodriguez', size='5'),
-                rx.text('@otakugodie', size='3')
+                rx.heading(
+                    'Diego Rodriguez', 
+                    size='5'
+                ),
+                rx.text(
+                    '@otakugodie', 
+                    size='3',
+                    margin_top='0px !important'
+                )
             )
-        ),
+        )
+    ),
         rx.text('I\'m business developer, product leader')
     )
 
