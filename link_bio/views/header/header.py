00e61abed2b2708fc22059d62440d5d82561d77e
otakugodie <95259091+otakugodie@users.noreply.github.com>
2024-07-01 12:39:26 +0100

    Adjust header.py

--- link_bio/views/header/header.py
+++ link_bio/views/header/header.py
@@ -1,23 +1,24 @@
 import reflex as rx
 
 def header() -> rx.Component:
-    return (rx.vstack(
-        rx.hstack(
-            rx.avatar(fallback="DR", size='5'),
-            rx.vstack(
-                rx.heading(
-                    'Diego Rodriguez', 
+    return rx.vstack(
+            rx.hstack(
+                rx.avatar(
+                    fallback="DR", 
                     size='5'
                 ),
-                rx.text(
-                    '@otakugodie', 
-                    size='3',
-                    margin_top='0px !important'
+                rx.vstack(
+                    rx.heading(
+                        'Diego Rodriguez', 
+                        size='5'
+                    ),
+                    rx.text(
+                        '@otakugodie', 
+                        size='3',
+                        margin_top='0px !important'
+                    )
                 )
-            )
+            ),
+            rx.text('I\'m business developer, product leader'),
+            align_items='start'
         )
-    ),
-        rx.text('I\'m business developer, product leader')
-    )
-
-    
