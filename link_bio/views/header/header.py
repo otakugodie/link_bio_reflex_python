31fae13fdeb6045268856a69f50dc02e58c030cc
otakugodie <95259091+otakugodie@users.noreply.github.com>
2024-07-02 22:27:28 +0100

    Add link_icon

--- link_bio/views/header/header.py
+++ link_bio/views/header/header.py
@@ -1,4 +1,6 @@
 import reflex as rx
+from link_bio.components.link_icon import link_icon
+from link_bio.styles.styles import Size as Size
 
 def header() -> rx.Component:
     return rx.vstack(
@@ -16,9 +18,16 @@ def header() -> rx.Component:
                         '@otakugodie', 
                         size='3',
                         margin_top='0px !important'
-                    )
+                    ),
+                    rx.hstack(
+                        link_icon('https://x.com/otakugodie'),
+                        link_icon('https://x.com/otakugodie'),
+                        link_icon('https://x.com/otakugodie')
+                    ),
+                    align_items='start'
                 )
             ),
             rx.text('I\'m business developer, product leader'),
+            spacing='7',
             align_items='start'
         )
