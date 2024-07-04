cabe486d424039a85656a0c5d74c635bb916edd5
otakugodie <95259091+otakugodie@users.noreply.github.com>
2024-07-04 19:44:36 +0100

    Add flex, bug with spacers

---
+++ link_bio/components/info_text.py
@@ -0,0 +1,12 @@
+import reflex as rx
+from link_bio.styles.styles import Size as Size
+
+def info_text(title: str, body: str) -> rx.Component:
+    return rx.box(
+        rx.text(
+            title,
+            as_='span',
+            color_scheme='ruby'
+        ),
+        f" {body}"
+    )
--- link_bio/views/header/header.py
+++ link_bio/views/header/header.py
@@ -1,5 +1,6 @@
 import reflex as rx
 from link_bio.components.link_icon import link_icon
+from link_bio.components.info_text import info_text
 from link_bio.styles.styles import Size as Size
 
 def header() -> rx.Component:
@@ -27,6 +28,16 @@ def header() -> rx.Component:
                     align_items='start'
                 )
             ),
+
+            rx.flex(
+                info_text('+12', 'years of experience'),
+                rx.spacer(),
+                info_text('+12', 'years of experience'),
+                width='100%'
+            ),
+            
+            
+            
             rx.text('I\'m business developer, product leader'),
             spacing='7',
             align_items='start'
